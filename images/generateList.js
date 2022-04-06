bebc248ef9e10856280a444ebe03d834d4b76ebc
Maxime Durand <durandmaxime1931@gmail.com>
2022-04-06 19:17:12 -0400

    Added Stylegan3 generated art.

--- images/generateList.js
+++ images/generateList.js
@@ -2,10 +2,17 @@ const fs = require('fs');
 
 fs.readdir('./images', (err, files) => {
     files = files.filter(file => !file.endsWith(".js") && !file.endsWith(".json"));
-    const list = files.map(file => ({
-        title: file.split('.').slice(0, -1).join('.'),
-        file,
-    }));
-    let json = JSON.stringify({images:list}, null, 4);
+    const list = files.map(file => {
+        const true_filename = file.split('.').slice(0, -1).join('.')
+        let written_filename = true_filename
+        if (true_filename.includes("seed")) {
+            written_filename = "StyleGAN3_" + written_filename
+        }
+        return ({
+            title: written_filename,
+            file,
+        })
+    });
+    let json = JSON.stringify({images: list}, null, 4);
     fs.writeFileSync('./images/images.json', json);
 });
